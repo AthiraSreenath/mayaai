e3b28008ccb2ef16cf3399ef84148a2e9bc45902
Sreenath Somarajapuram <somarajapuram@gmail.com>
2024-04-27 19:51:41 -0700

    Hosting UI in the backend server

--- backend/DEV_README.md
+++ backend/DEV_README.md
@@ -11,17 +11,3 @@ pip install -r requirements.txt
 ```
 uvicorn server:app --reload
 ```
-
-## Update UI
-UI code is in the `/frontend` directory. A production build of the UI is available in `/backend/public` directory for easy startup... it must be kept updated.
-
-In `./frontend`
-```
-npm run build
-```
-
-In `./backend`
-```
-rm -rf public/
-cp -r ../frontend/build public
-```
--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,19 @@
+from os import path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
 
 
 app = FastAPI()
+ui_path = path.join(path.dirname(__file__), '../frontend/build/')
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def index():
+    return FileResponse(path.join(ui_path, "index.html"))
+
+@app.exception_handler(404)
+async def exception_404_handler(request, exc):
+    return FileResponse(path.join(ui_path, "index.html"))
+
+app.mount("/", StaticFiles(directory=ui_path), name="ui")
