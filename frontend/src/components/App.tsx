b5f9a51a5967790e972aa77c8f35eb346b83b802
Sreenath Somarajapuram <somarajapuram@gmail.com>
2024-04-27 17:40:50 -0700

    Adding base layout + router with pages models & datasets

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
 
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet,
+  } from "react-router-dom";
+
 import ThemeSetter from './ThemeSetter';
 import { ContextHandler } from './AppContext';
+
+
+export enum Routes {
+    Home = "/",
+    Models = "/models",
+    Datasets = "/datasets"
+}
+
+const AppLayout: () => JSX.Element = () => {
+    return (
+        <div className="App">
+            <Outlet />
+        </div>
+    );
+}
+
+const router = createBrowserRouter([
+    {
+        path: Routes.Home,
+        Component: AppLayout,
+        children: [
+            {
+                path: Routes.Models,
+                element: <h1>Models Page</h1>,
+            },
+            {
+                path: Routes.Datasets,
+                element: <h1>Datasets Page</h1>,
+            },
+        ]
+    },
+]);
 
 function App() {
   return (
     <ContextHandler>
         <ThemeSetter>
-            <div className="App">
-                <header className="App-header">
-                    MayaAI
-                </header>
-            </div>
+            <RouterProvider router={router} />
         </ThemeSetter>
     </ContextHandler>
   );
