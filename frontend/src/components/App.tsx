5b224c79339b6f982ceb7dcae5621f53b4a70ee1
Sreenath Somarajapuram <somarajapuram@gmail.com>
2024-04-27 17:51:55 -0700

    Adding invalid path redirect

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
             },
             {
                 path: "*",
-                element: <Navigate to="/" replace />
+                element: <Navigate to={Routes.Models} replace />
             }
         ]
     },
